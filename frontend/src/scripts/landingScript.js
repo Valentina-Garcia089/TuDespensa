aadce223e78b694f012e912891ed96e147ed005e
Valentina Garcia <valentinagarciag089@gmail.com>
2025-10-19 21:55:49 -0500

    Pantalla 1 de registro de usuario

--- frontend/src/scripts/landingScript.js
+++ frontend/src/scripts/landingScript.js
@@ -6,7 +6,18 @@ const profileButton = document.getElementById("open-profile-options");
 const userMenu = document.querySelector(".header__user-menu");
 
 // Cuando alguien haga click, se ejecutará lo que está dentro de la función
-
 profileButton.addEventListener("click", () =>{
     userMenu.classList.toggle("show"); // Agrega la clase show o la quita si ya la tiene
+})
+
+
+// Redireccionamiento a la página de registro
+const btnRegistro = document.getElementById("header__start-button");
+btnRegistro.addEventListener("click", () => {
+    window.location.href = "register.html";
+})
+
+const btnRegistro2 = document.getElementById("benefits__start-button");
+btnRegistro2.addEventListener("click", () => {
+    window.location.href = "register.html";
 })
