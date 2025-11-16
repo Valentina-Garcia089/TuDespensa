d42805af6c2fad5437a3dc68a37b3af35a6d850b
Valentina Garcia <valentinagarciag089@gmail.com>
2025-11-16 01:05:44 -0500

    Metodo de pago por tarjeta de crédito o debito agregado

--- frontend/src/scripts/addPaymentScript.js
+++ frontend/src/scripts/addPaymentScript.js
@@ -17,6 +17,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const returnBtn = document.querySelector(".return__button2");
     const darkLayer = document.querySelector(".dark__layer");
 
+    const addCreditCard = document.getElementById("credit__card-btn");
+    const hiddenForm = document.getElementById("hidden-method");
+
     function openModal(){
         darkLayer.classList.add("active");
         hiddenmodal.classList.add("open");
@@ -29,9 +32,24 @@ document.addEventListener("DOMContentLoaded", () => {
         document.body.style.overflow = "";
     }
 
+    function openForm(){
+        darkLayer.classList.add("active");
+        hiddenmodal.classList.remove("open"); /*Cerrar el modal anterior*/ 
+        hiddenForm.classList.add("open");
+        document.body.style.overflow = "hidden";
+    }
+
+    function closeForm(){
+        darkLayer.classList.remove("active");
+        hiddenForm.classList.remove("open");
+        document.body.style.overflow = "";
+    }
+
     addNewBtn.addEventListener("click", openModal);
     darkLayer.addEventListener("click", closeModal);
     returnBtn.addEventListener("click", closeModal);
+
+    addCreditCard.addEventListener("click", openForm);
     document.addEventListener("keydown", (e) =>{
         if(e.key === "Escape"){
             closeModal();
